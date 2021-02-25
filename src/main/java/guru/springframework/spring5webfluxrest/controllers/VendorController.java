44c75e3f8b5b9865494afec97d625ebf130f6c1d
Developer <jjstrain68@gmail.com>
2021-02-25 14:40:26 +0000

    Added update vendor and category plus unit tests

--- src/main/java/guru/springframework/spring5webfluxrest/controllers/VendorController.java
+++ src/main/java/guru/springframework/spring5webfluxrest/controllers/VendorController.java
@@ -34,4 +34,9 @@ public class VendorController {
         return vendorRepository.saveAll(vendorStream).then();
     }
 
+    @PutMapping("/api/v1/vendors/{id}")
+    Mono<Vendor> update(@PathVariable String id, @RequestBody Vendor vendor) {
+        vendor.setId(id);
+        return vendorRepository.save(vendor);
+    }
 }
